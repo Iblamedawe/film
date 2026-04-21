06ceca175e5f40eca1e7e9d7e1081f2d63e6ae5f
Iblamedawe <puskasdavid2008@gmail.com>
2026-04-21 10:21:58 +0200

    yesnt

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const movies = document.querySelectorAll('.movie');
+
+    movies.forEach(movie => {
+        movie.addEventListener('click', () => {
+            movie.classList.toggle('expanded');
+        });
+    });
+});
